fbe67191281c11b0cd09b28b96dafca23ce36fed
Domorapter <166681538+Domorapter@users.noreply.github.com>
2024-04-23 15:50:22 -0500

    Update cart.py
    added a :

--- initialProjectFiles/cart.py
+++ initialProjectFiles/cart.py
@@ -13,7 +13,7 @@ class Cart:
         databaseName: string
         
     def cartMenu(user): ## receving user class to track userID to cart
-        while(1)
+        while(1):
             print("Cart Information Menu:")
             print("0. Leave Cart Information")
             print("1. View Cart")
