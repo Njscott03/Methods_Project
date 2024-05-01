f81da1218bf64e462e93ac64657d44a3d605c2b7
Domorapter <166681538+Domorapter@users.noreply.github.com>
2024-04-30 19:00:05 -0500

    Update history.py
    Minor syntax changes, still broken

--- initialProjectFiles/history.py
+++ initialProjectFiles/history.py
@@ -1,7 +1,9 @@
-rom user import *
+from user import *
 from inventory import *
+import random
 import sqlite3
 import sys
+
 
 class OrderHistory:
 ## belongs to Dakota
@@ -9,10 +11,8 @@ class OrderHistory:
 ## to get basic order information
     
     def __init__(self):
-        databaseName = "OrderDB.db"
-        secondDatabaseName = "OrderItems.db"
-        self.orderDatabaseName = databaseName
-        self.itemsDatabaseName = secondDatabaseName
+        self.orderDatabaseName = "methods.db"
+        
     
   
         ## table of orders
@@ -31,80 +31,60 @@ class OrderHistory:
 
         orderID = input("Input Order ID:")
         ## selects all information
-        query = "SELECT ItemNumber,Cost,Date FROM Orders WHERE UserID ='" + userID + "'"
+        query = "SELECT * FROM Orders WHERE OrderNumber ='" + orderID + "'"
         print("Orders (ItemNumber, Cost, Date):")
         
         cursor.execute(query)
-        print(cursor.fetchall()) ## i want to format the print...but it seems like an ungodly amount of work
+        results = cursor.fetchall()
+        
+        print(results) ## i want to format the print...but it seems like an ungodly amount of work
         cursor.close()
 
 
         ## table of items
     def viewOrder(self,userID, orderID): 
-        a
+        print("inside viewOrder")
         ##calls some function from inventory to display orderItems
+
         
     def createOrder(self,userID, quantity, cost, date):
-        a
+        print("inside createOrder")
+        try:
+            connection = sqlite3.connect(self.orderDatabaseName)
+
+        except:
+            print("Failed database connection.")
+
+            ## exits the program if unsuccessful
+            sys.exit()
+        cursor = connection.cursor()
         
-    def addOrderItems(self, userID, orderID):
-        a
+        orderNum = random.randint(100000, 999999)
+        query = "SELECT OrderNumber FROM Orders WHERE userID='" + userID + "'"
+        cursor.execute(query)
+        OrderNumTuple = cursor.fetchall()
 
 
-    def historyMenu(self,userID,inventory,history):
-        print("inside gistoa")
-        while(1):
-            print()
-            print("Cart Information Menu:")
-            print("0. Leave History Information")
-            print("1. View History")
-            print("2. View Order")
-            print("3. Create Order")
-            print("4. Add to Order")
-
-            ## userID = user.userID()
-            userID = "123A567" ## this is purely a random ID that
-                               ## wll be removed after User works
-            
+        ## what are the odds the order number is the same for one userID after regeneration
+        for item in OrderNumTuple:
+            if(orderNum,) == item:
+                orderNum = random.randint(100000, 999999)
+        
+        query = "INSERT INTO Orders (OrderNumber, UserID, itemNumber) VALUES (?,?,?)"
            
-            historyOption = input("Enter your menu choice: ")
-
-            ## Leave Cart Information
-            if(historyOption == "0"):
-                break
-    
-            ## View History
-            if(historyOption == "1"):
-                history.viewHistory(userID)   
-                
-            ## View Order
-            if(historyOption == "2"):
-                history.viewOrder(userID, orderID)
-                
-            ## Create Order
-            if(historyOption == "3"):
-                ISBN = input("What is the ISBN:")
-                quantity = input("What is the quantity you want to remove:")
-
-                cart.removeFromCart(ISBN,userID,quantity)
-    
-            ## Add to Order
-            if(historyOption == "4"):
-                cart.checkOut(userID)
-
-
+        ## definitly unfinished. i wanted to have cart play well with the function but....not happening
         
-        print("Successfully Left Cart Information.")
-        print()
-        print("ending history.py")
+
+        ## grab current date
+        
+    def addOrderItems(self,orderID,ISBN, quantity):
+        print("inside addOrderItems")
+        print(ISBN)
+        ## would call a query to add ISBN and Quantity to OrderItems, linked by orderNumber
 
 
-""" TO DO--------------------------------------------
--change how view order works
--make createOrder
--make addOrderItems
-"""
-def main():
+
+    def historyMenu(self,userID,inventory):
         print("Welcome")
         user = User()
         history = OrderHistory()
@@ -164,7 +144,63 @@ def main():
         cursor.close()
         print("\n--------------------------------------------")
 
-                
-        history.historyMenu(userID,inventory,history)
 
-main()
+
+
+
+
+        print("inside History Menu")
+        while(1):
+            print()
+            print("Cart Information Menu:")
+            print("0. Leave History Information")
+            print("1. View History")
+            print("2. View Order")
+            print("3. Create Order")
+            print("4. Add to Order")
+
+            ## userID = user.userID()
+            userID = "123A567" ## this is purely a random ID that
+                               ## wll be removed after User works
+            
+           
+            historyOption = input("Enter your menu choice: ")
+
+            ## Leave Cart Information
+            if(historyOption == "0"):
+                break
+    
+            ## View History
+            if(historyOption == "1"):
+                history.viewHistory(userID)   
+                
+            ## View Order
+            if(historyOption == "2"):
+                history.viewOrder(userID, orderID)
+                
+            ## Create Order
+            if(historyOption == "3"):
+                
+                cart.createOrder(userID,quantity,cost,date)
+    
+            ## Add to Order
+            if(historyOption == "4"):
+                cart.checkOut(userID)
+
+
+        
+        print("Successfully Left Cart Information.")
+        print()
+        print("ending history.py")
+
+
+""" TO DO--------------------------------------------
+-change how view order works
+-make createOrder
+-make addOrderItems
+"""
+
+history = OrderHistory()
+inventory = Inventory()
+##history.historyMenu("15-5444",inventory)
+
